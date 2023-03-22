2598a693029b1eccbf89dc0d01c7e42471df7b36
Dissmor <manamsa.2004@mail.ru>
2023-03-22 15:52:59 +0600

    Cosmetic changes

--- Parser.py
+++ Parser.py
@@ -5,8 +5,7 @@ import openpyxl
 
 def parse():
     url = 'https://omsk.hh.ru/vacancies/programmist?hhtmFromLabel=rainbow_profession&hhtmFrom=main'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2'}
     page = requests.get(url, headers=headers)
     print(page.status_code)
 
